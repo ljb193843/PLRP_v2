fdc19a54ecfd9c170509eeae14de16c02568d521
Unkwounxx <jiflorescarrasco@hotmail.com>
2017-09-12 11:24:11 -0300

    GML con Depositos

--- PLRP_V2_1/Code/GML_generator.hpp
+++ PLRP_V2_1/Code/GML_generator.hpp
@@ -11,6 +11,8 @@ void Generate_GML(unmap_C Contenedores, unmap_D Depositos,ofstream& output){
    output << "\t" << "label	\"\"" << "\n";
    output << "\t" << "directed	1" << "\n";
 
+   int contC = 0;
+
    for(c_it=Contenedores.begin();c_it!=Contenedores.end();c_it++){
 
       string id = c_it->second->get_id();
@@ -41,7 +43,47 @@ void Generate_GML(unmap_C Contenedores, unmap_D Depositos,ofstream& output){
       output << "\t" << "\t" << "model" << "\t" << "\"null\"" << "\n";
       output << "\t" << "\t" << "]" << "\n";
       output << "\t" << "]" << "\n";
+      contC++;
 
    }
+
+   int contD = 0;
+
+   for(d_it=Depositos.begin();d_it!=Depositos.end();d_it++){
+
+      string id = d_it->second->get_id();
+      string id2 = id.erase(0,1);
+      double X = d_it->second->get_X();
+      double Y = d_it->second->get_Y();
+
+      output << "\t" << "node" << "\n";
+      output << "\t" << "[" << "\n";
+      output << "\t" << "\t" << "id "+id2 << "\n";
+      output << "\t" << "\t" << "label \""+id2+"\"" << "\n";
+      output << "\t" << "\t" << "graphics" << "\n";
+      output << "\t" << "\t" << "[" << "\n";
+      output << "\t" << "\t" << "\t" << "x" << "\t" << X << "\n";
+      output << "\t" << "\t" << "\t" << "y" << "\t" << Y << "\n";
+      output << "\t" << "\t" << "\t" << "w" << "\t" << "30.0" << "\n";
+      output << "\t" << "\t" << "\t" << "h" << "\t" << "30.0" << "\n";
+      output << "\t" << "\t" << "\t" << "type" << "\t" << "\"circle\"" << "\n";
+      output << "\t" << "\t" << "\t" << "raisedBorder" << "\t" << "0" << "\n";
+      output << "\t" << "\t" << "\t" << "fill" << "\t" << "\"#0000FF\"" << "\n";
+      output << "\t" << "\t" << "\t" << "outline" << "\t" << "\"#000000\"" << "\n";
+      output << "\t" << "\t" << "]" << "\n";
+      output << "\t" << "\t" << "LabelGraphics" << "\n";
+      output << "\t" << "\t" << "[" << "\n";
+      output << "\t" << "\t" << "text" << "\t" << "\""+id+"\"" << "\n";
+      output << "\t" << "\t" << "fontSize" << "\t" << "12" << "\n";
+      output << "\t" << "\t" << "fontName" << "\t" << "\"Dialog\"" << "\n";
+      output << "\t" << "\t" << "model" << "\t" << "\"null\"" << "\n";
+      output << "\t" << "\t" << "]" << "\n";
+      output << "\t" << "]" << "\n";
+      contD++;
+
+   }
+
+   cout << "Depositos: " << contD << endl;
+   cout << "Contenedores: " << contC << endl;
 
 }
