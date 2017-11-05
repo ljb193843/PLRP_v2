b8629812ffc3f83f0d15d010cfc354d95726e411
Unkwounxx <jiflorescarrasco@hotmail.com>
2017-11-05 02:00:53 -0300

    Creacion de rutas

--- PLRP_V2_3/Code/Greedy_Rute.hpp
+++ PLRP_V2_3/Code/Greedy_Rute.hpp
@@ -1,3 +1,38 @@
-void Greedy_Rute(){
+void Greedy_Rute(string dia,unmap_st_unmap costos,vector<unmap_C> &Conts_dias,unmap_R &Rutas,unmap_D &Depositos,int &contR){
+
+   //cout << Conts_dias.size() << endl;
+
+   unmap_C All_Conts;
+
+
+   for(int i=0;i<Conts_dias.size();i++){
+
+      unmap_C_it c_it;
+
+      for(c_it=Conts_dias[i].begin();c_it!=Conts_dias[i].end();c_it++){
+
+         All_Conts[c_it->first] = c_it->second;
+
+      }
+
+   }
+   //Buscar los id's del contenedor y deposito mas cercanos entre si
+   //asginarlos al par ids
+   pair<string,string> ids = Cont_Dept_MasCercanos(costos,All_Conts,Depositos);
+
+   string idD = ids.first;// id del deposito
+   string idC1 = ids.second;//id del contenedor
+
+   Ruta *R = new Ruta;
+
+
+
+
+
+
+
+
+
+
 
 }
